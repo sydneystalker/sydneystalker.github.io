61e4925a1dfae0266d42fe43bd9d4789be186228
Sydney <sstalker@csumb.edu>
2025-11-08 19:28:54 -0500

    Refactor HTML structure and enhance form validation in US Geography Quiz

--- cst336/hw2/js/script.js
+++ cst336/hw2/js/script.js
@@ -10,11 +10,28 @@
 //Event Listener
 document.querySelector("button").addEventListener("click", gradeQuiz);
 
+//Functions
+function isFormValid(){
+    let isValid = true;
+    if(document.querySelector("#q1").value == ""){
+        isVaild = false;
+        document.querySelector("#validationFdbk").innerHTML = "Question 1 was not answered";
+    }
+    return isValid;
+}
+
 
 function gradeQuiz(){
  console.log("Grading quiz…");
- let q1Response = document.querySelector("#q1").value;
+ document.querySelector("#validationFdbk").innerHTML = "";
+if(!isFormValid()){
+ return;
+}
+
+let q1Response = document.querySelector("#q1").value;
  console.log(q1Response);
+
+
 }
 
 
